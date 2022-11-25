01f95461da6a379256122aa222e5c612080e3f4d
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-11-25 15:34:41 -0300

    remove folder separation

--- back-end/node_modules/@trpc/server/dist/adapters/node-http/index.mjs
+++
@@ -1,7 +0,0 @@
-export { n as nodeHTTPRequestHandler } from '../../nodeHTTPRequestHandler-12223ae5.mjs';
-import '../../resolveHTTPResponse-1829da6c.mjs';
-import '../../config-1bc170ad.mjs';
-import '../../TRPCError-802c850e.mjs';
-import '../../codes-52c11119.mjs';
-import '../../index-972002da.mjs';
-import '../../transformTRPCResponse-7a73a2df.mjs';
